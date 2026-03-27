7c71404bd9efd1d327300981c3f3faad390bfb34
Shravan Mani <shravan.mani@hotmail.com>
2026-03-26 20:27:14 -0400

    Update Vite config for base path and aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/PinPoint-Duel/',
+  base: './', 
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '.'),
+      '@': path.resolve(__dirname, './src'),
     },
-  },
-  server: {
-    hmr: process.env.DISABLE_HMR !== 'true',
   },
 });
